e8f2e1d9af5d9adbe75743029ca9f00a868f5aca
bharathKashyap <bharath111.spt@gmail.com>
2021-01-27 10:12:08 +0530

    first Commit

---
+++ src/com/leetcode/devevlop/daily/ValidParanthesis.java
@@ -0,0 +1,38 @@
+package com.leetcode.devevlop.daily;
+
+import java.util.Stack;
+
+public class ValidParanthesis {
+
+	public static boolean isValid(String s) {
+
+		Stack stack = new Stack();
+		s.trim();
+		for (int i = 0; i < s.length(); i++) {
+
+			String character = String.valueOf(s.charAt(i));
+			if (stack.isEmpty()) {
+				stack.push(character);
+			} else if (character.equalsIgnoreCase("}") && stack.peek().toString().equalsIgnoreCase("{")
+					|| character.equalsIgnoreCase("]") && stack.peek().toString().equalsIgnoreCase("[")
+					|| character.equalsIgnoreCase(")") && stack.peek().toString().equalsIgnoreCase("(")) {
+				stack.pop();
+			} else {
+				stack.push(character);
+			}
+		}
+		if (stack.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		String input = "{{}[][[[]]]}";
+//		String input = "(])";
+		boolean result = isValid(input);
+
+		System.out.println(result);
+	}
+
+}
---
+++ src/com/leetcode/devevlop/ds/array/SubsetProblem.java
@@ -0,0 +1,42 @@
+package com.leetcode.devevlop.ds.array;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class SubsetProblem {
+
+	public List<List<Integer>> subsets(int[] nums) {
+
+		List<List<Integer>> endPairList = new ArrayList<List<Integer>>();
+		Set<Integer> powerList = new HashSet<>();
+		subsetsHelper(endPairList, new ArrayList<>(), nums, 0);
+
+		return endPairList;
+
+	}
+
+	private void subsetsHelper(List<List<Integer>> list, List<Integer> resultList, int[] nums, int start) {
+		list.add(new ArrayList<>(resultList));
+		for (int i = start; i < nums.length; i++) {
+			// add element
+			resultList.add(nums[i]);
+			// Explore
+			subsetsHelper(list, resultList, nums, i + 1);
+			// remove
+			resultList.remove(resultList.size() - 1);
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		SubsetProblem problem = new SubsetProblem();
+		int[] tCase1 = { 1, 2, 3 };
+		List<List<Integer>> outValue = problem.subsets(tCase1);
+		System.out.println(outValue);
+
+	}
+
+}
