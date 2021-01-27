548a4ae38ecbd599c2285a28f65206a5b813c5ba
bharathKashyap <bharath111.spt@gmail.com>
2021-01-27 11:01:28 +0530

    5th commit

--- src/com/leetcode/devevlop/daily/ValidParanthesis.java
+++ src/com/leetcode/devevlop/daily/ValidParanthesis.java
@@ -22,7 +22,7 @@ public class ValidParanthesis {
 					stack.push(character);
 				}
 			} catch (Exception e) {
-				System.out.println("REMOVED THIS LINE, REVERT REQD :" + e);
+				System.out.println("REMOVE THIS LINE FOR NOW");
 			}
 		}
 		if (stack.isEmpty()) {
